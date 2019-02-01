c150bcd040dbbd6d63885bf28dd33cea810e81eb
Peter Georgiev <pesho10@yahoo.com>
2019-02-01 09:12:08 +0100

    Introducing leaflet onload event

--- src/maniakalen/maps/resources/js/leaflet_init.js
+++ src/maniakalen/maps/resources/js/leaflet_init.js
@@ -15,7 +15,9 @@ $(document).ready(function() {
         options.attribution = options.attribution || 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 
         var map = window.leaflet.map = L.map(mapId).setView(coords, zoom);
-
+        map.on('load', function() {
+            $('body').trigger('leaflet.map.loaded');
+        });
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
             attribution: options.attribution,
             maxZoom: 18,
